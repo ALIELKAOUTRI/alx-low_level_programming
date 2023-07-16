0cb17624416faacd6fc0ff6b5538cfe93b1a37b1
ali elkaoutri <alielkaoutri@gmail.com>
2023-07-16 10:29:39 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,8 @@
-#include <stdio.h>
-
-/**
- * main - Entry point of the program
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-    int sizeOFachar = 1;
-    printf("size of a char: %d byte(s)", sizeOFachar);
-
-    int sizeOFanint = 4;
-    printf("\nsize of an int: %d byte(s)", sizeOFanint);
-
-    int sizeOFalongint = 4;
-    printf("\nsize of a long int: %d byte(s)", sizeOFalongint);
-
-    int sizeOFfloat = 4;
-    printf("\nsize of a float: %d byte(s)", sizeOFfloat);
-
-    return (0);
-}}
+#include<stdio.h>
+int main(){
+    printf("size of a char : %d byte(s)", sizeof(char));
+    printf("\nsize of an int : %d byte(s)", sizeof(int));
+    printf("\nsize of a long int : %d byte(s)", sizeof(long int));
+    printf("\nsize of a float : %d byte(s)", sizeof(float));
+    return 0;
+}
