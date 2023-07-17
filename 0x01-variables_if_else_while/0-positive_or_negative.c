0d4980fe09689764605caa4f9f332fb7aaee3065
ali elkaoutri <alielkaoutri@gmail.com>
2023-07-17 09:01:29 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,18 +9,18 @@
  */
 int main(void)
 {
-    srand(time(NULL));  // Initialize the random number generator
+    srand(time(NULL));  /* Initialize the random number generator */
 
-    int n = rand() % 100;  // Generate a random number between 0 and 99
+    int n;
+    n = rand() % 100;  /* Generate a random number between 0 and 99 */
 
     if (n > 0) {
-        printf("%d is positive", n);
+        printf("%d is positive\n", n);
     } else if (n == 0) {
-        printf("%d is zero", n);
+        printf("%d is zero\n", n);
     } else if (n < 0) {
-        printf("%d is negative", n);
+        printf("%d is negative\n", n);
     }
 
     return 0;
 }
-
